89c925a937c33400b8c5d96c8fbf72a05dbf9f11
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-08-30 11:30:52 -0300

    Update 16-config.sh

--- scripts/16-config.sh
+++ scripts/16-config.sh
@@ -10,3 +10,9 @@ cp -R /$HOME/i3wm-debian/config/home/* "/$HOME/"
 
 cp -f /etc/xdg/dunst/dunstrc ~/.config/dunst/dunstrc
 
+# Other
+
+cd /$HOME/.config/i3/scripts/
+
+chmod +x chmod.sh fehbg.sh dunst_reload.sh spotify_debug.sh
+
