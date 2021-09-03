bd78ba44bd59d51a0a586dfaa9be3924fbce4ece
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-09-03 00:25:13 -0300

    Add files via upload

---
+++ config/.config/i3/scripts/upgrade.sh
@@ -0,0 +1,7 @@
+sudo apt-get update && sudo apt-get upgrade -y ; sudo apt-get dist-upgrade -y
+
+flatpak update
+
+flatpak upgrade
+
+sudo apt-get autoremove -y && sudo apt-get autoclean -y && sudo apt-get clean -y
