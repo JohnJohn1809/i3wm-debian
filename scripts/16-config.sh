e56a1439bec4883367c6b2f4cae41a587814150f
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-09-27 17:49:19 -0300

    Update 16-config.sh

--- scripts/16-config.sh
+++ scripts/16-config.sh
@@ -6,6 +6,8 @@ cp -R /$HOME/i3wm-debian/config/.config/* "/$HOME/.config/"
 
 cp -R /$HOME/i3wm-debian/config/home/* "/$HOME/"
 
+cp -R /$HOME/i3wm-debian/config/etc/* "/etc/"
+
 # Dunst
 
 cp -f /etc/xdg/dunst/dunstrc ~/.config/dunst/dunstrc
