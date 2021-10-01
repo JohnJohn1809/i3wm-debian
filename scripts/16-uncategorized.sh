fd7416b5d198d615242a86e28a7222d95f8f9595
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-09-30 21:41:53 -0300

    Update 16-uncategorized.sh

--- scripts/16-uncategorized.sh
+++ scripts/16-uncategorized.sh
@@ -3,3 +3,9 @@
 sudo apt install gnome-calculator -y
 sudo apt install numlockx -y
 sudo apt install neofetch -y
+
+# Spotify
+
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client -y
