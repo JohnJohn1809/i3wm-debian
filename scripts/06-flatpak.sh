77013d1947d528d2125e4b7636f8135bcd1ba194
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-10-20 18:10:49 -0300

    Update 06-flatpak.sh

--- scripts/06-flatpak.sh
+++ scripts/06-flatpak.sh
@@ -16,9 +16,9 @@ flatpak install flathub org.mozilla.firefox
 
 # Social
 
-flatpak install flathub org.telegram.desktop
+#flatpak install flathub org.telegram.desktop
 
-flatpak install flathub com.discordapp.Discord
+#flatpak install flathub com.discordapp.Discord
 
 # Text Edit
 
