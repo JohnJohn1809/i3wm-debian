05e6cf615ecc5e323b88027f5929eac6a3792283
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-10-30 18:04:31 -0300

    Update startup.sh

--- config/.config/i3/scripts/startup.sh
+++ config/.config/i3/scripts/startup.sh
@@ -6,7 +6,7 @@ i3-msg 'workspace 10; append_layout ~/.config/i3/layouts/workspace_10.json'
 
 # Web Browser
 
-i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox'
+i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox https://myaccount.mercadolivre.com.br/my_purchases/list#nav-header https://editorasundermann.com.br/livro/96-marx-e-a-hist%C3%B3ria https://www.ligamagic.com.br/?view=colecao/colecao https://www.reddit.com/r/unixporn/'
 
 i3-msg 'exec --no-startup-id brave-browser https://www.youtube.com/feed/subscriptions https://www.hltv.org/matches'
 
@@ -20,7 +20,9 @@ i3-msg 'exec --no-startup-id thunar'
 
 # Social
 
-i3-msg 'exec --no-startup-id flatpak run org.telegram.desktop'
+i3-msg 'exec --no-startup-id ~/.appimage/telegram/exec/telegram'
+
+#i3-msg 'exec --no-startup-id flatpak run org.telegram.desktop'
 
 i3-msg 'exec --no-startup-id flatpak run com.discordapp.Discord'
 
