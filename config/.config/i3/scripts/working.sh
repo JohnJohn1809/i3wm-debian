32528b291d73a54dd9f8fd4d22e36643e3b2d6b1
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-11-08 20:06:22 -0300

    Add files via upload

---
+++ config/.config/i3/scripts/working.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Layout
+
+i3-msg 'workspace 3; append_layout ~/.config/i3/layouts/workspace_03_working.json'
+
+# Web Browser
+
+i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox'
+
+# Text Editor
+
+i3-msg 'exec --no-startup-id libreoffice /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/style/new/13.2-Referências.odt'
+
+# File Manager
+
+i3-msg 'exec --no-startup-id thunar ~/Downloads/ ~/Documents/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/99-images/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/07-atividades/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/08-anexos/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/'
