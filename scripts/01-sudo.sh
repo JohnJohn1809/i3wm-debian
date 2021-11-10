6d94b5b988573c6fa5ef6e53c5babd33d92e8853
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-11-10 18:49:19 -0300

    Delete scripts directory

--- scripts/01-sudo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-su -
-
-apt install sudo -y
-
-usermod -aG sudo johni3
-
-systemctl reboot
--- scripts/02-sources-list.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo cp -R /$HOME/i3wm-debian/config/etc/apt/* "/etc/apt/"
-
-sudo apt update
-
