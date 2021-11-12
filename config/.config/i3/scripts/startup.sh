016f48953dcc9a67be0375b970b3c970dda7a6a4
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-11-12 19:28:18 -0300

    Update startup.sh

--- config/.config/i3/scripts/startup.sh
+++ config/.config/i3/scripts/startup.sh
@@ -8,7 +8,7 @@ i3-msg 'workspace 10; append_layout ~/.config/i3/layouts/workspace_10.json'
 
 # Web Browser
 
-i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox https://myaccount.mercadolivre.com.br/my_purchases/list#nav-header https://editorasundermann.com.br/livro/96-marx-e-a-hist%C3%B3ria https://editorasundermann.com.br/livro/185-anu%C3%A1rio-estat%C3%ADstico-do-ilaese--2021# https://rastreamento.correios.com.br/app/index.php https://www.ligamagic.com.br/?view=colecao/colecao https://www.reddit.com/r/unixporn/new/ https://gleam.io/4vRJv/sorteio-black-november-kit-upgrade-terabyteshop https://gleam.io/pp41G/sorteio-pos-black-november-pc-gamer-terabyteshop'
+i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox https://myaccount.mercadolivre.com.br/my_purchases/list#nav-header https://editorasundermann.com.br/livro/96-marx-e-a-hist%C3%B3ria https://editorasundermann.com.br/livro/185-anu%C3%A1rio-estat%C3%ADstico-do-ilaese--2021# https://rastreamento.correios.com.br/app/index.php https://www.ligamagic.com.br/?view=colecao/colecao https://www.reddit.com/r/unixporn/new/ https://gleam.io/pp41G/sorteio-pos-black-november-pc-gamer-terabyteshop'
 
 i3-msg 'exec --no-startup-id brave-browser https://www.youtube.com/feed/subscriptions https://www.hltv.org/matches'
 
@@ -33,3 +33,7 @@ i3-msg 'exec --no-startup-id spotify Try --no-zygote'
 # Anki
 
 i3-msg 'exec --no-startup-id flatpak run net.ankiweb.Anki'
+
+# KeePassXC
+
+i3-msg 'exec --no-startup-id keepassxc'
