3c03535932fc7df66301f030b7103db074226103
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-11-17 15:19:39 -0300

    Update working.sh

--- config/.config/i3/scripts/working.sh
+++ config/.config/i3/scripts/working.sh
@@ -14,4 +14,4 @@ i3-msg 'exec --no-startup-id libreoffice /mnt/1BCDE0CC71679839/John/Documents/Ra
 
 # File Manager
 
-i3-msg 'exec --no-startup-id thunar ~/Downloads/ ~/Documents/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/99-images/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/07-atividades/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/08-anexos/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/'
+i3-msg 'exec --no-startup-id thunar ~/Downloads/ ~/Documents/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/99-images/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/07-atividades/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/conteúdo/08-anexos/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/portifolio/style/new/ /mnt/1BCDE0CC71679839/John/Documents/Random/santa-juliana/'
