fe1b3811e79474f5d9f58b859e54841833bbaa4f
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-11-18 14:57:09 -0300

    Update startup.sh

--- config/.config/i3/scripts/startup.sh
+++ config/.config/i3/scripts/startup.sh
@@ -4,11 +4,15 @@
 
 i3-msg 'workspace 3; append_layout ~/.config/i3/layouts/workspace_03.json'
 
+i3-msg 'workspace 9; append_layout ~/.config/i3/layouts/workspace_09.json'
+
 i3-msg 'workspace 10; append_layout ~/.config/i3/layouts/workspace_10.json'
 
 # Web Browser
 
-i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox https://myaccount.mercadolivre.com.br/my_purchases/list#nav-header https://editorasundermann.com.br/livro/96-marx-e-a-hist%C3%B3ria https://editorasundermann.com.br/livro/185-anu%C3%A1rio-estat%C3%ADstico-do-ilaese--2021# https://rastreamento.correios.com.br/app/index.php https://www.ligamagic.com.br/?view=colecao/colecao https://www.reddit.com/r/unixporn/new/ https://gleam.io/pp41G/sorteio-pos-black-november-pc-gamer-terabyteshop'
+i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox https://editorasundermann.com.br/livro/96-marx-e-a-hist%C3%B3ria https://editorasundermann.com.br/livro/185-anu%C3%A1rio-estat%C3%ADstico-do-ilaese--2021# https://rastreamento.correios.com.br/app/index.php https://www.ligamagic.com.br/?view=colecao/colecao https://www.reddit.com/r/unixporn/new/ https://gleam.io/pp41G/sorteio-pos-black-november-pc-gamer-terabyteshop'
+
+#i3-msg 'exec --no-startup-id flatpak run org.mozilla.firefox https://myaccount.mercadolivre.com.br/my_purchases/list#nav-header https://editorasundermann.com.br/livro/96-marx-e-a-hist%C3%B3ria https://editorasundermann.com.br/livro/185-anu%C3%A1rio-estat%C3%ADstico-do-ilaese--2021# https://rastreamento.correios.com.br/app/index.php https://www.ligamagic.com.br/?view=colecao/colecao https://www.reddit.com/r/unixporn/new/ https://gleam.io/pp41G/sorteio-pos-black-november-pc-gamer-terabyteshop'
 
 i3-msg 'exec --no-startup-id brave-browser https://www.youtube.com/feed/subscriptions https://www.hltv.org/matches'
 
