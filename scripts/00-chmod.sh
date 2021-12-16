679563e105587d0cc9cfc421e4bb69aead1889a2
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-12-16 13:16:55 -0300

    Update 00-chmod.sh

--- scripts/00-chmod.sh
+++ scripts/00-chmod.sh
@@ -2,4 +2,4 @@
 
 # Set Scripts Executables
 
-chmod +x 01-sources-list.sh 02-xorg-xdg.sh 03-set-xdg-paths.sh 04-depedences.sh 05-i3wm.sh 06-midia-managers.sh 07-sound-managers.sh 08-documents-managers.sh 09-archives-managers.sh 10-xfce-pack.sh 11-web-browser.sh 12-uncategorized.sh 13-flatpak.sh 14-appearance.sh 15-config.sh
+chmod +x 01-sources-list.sh 02-xorg-xdg.sh 03-set-xdg-paths.sh 04-depedences.sh 05-i3wm.sh 06-midia-managers.sh 07-sound-managers.sh 08-documents-managers.sh 09-archives-managers.sh 10-xfce-pack.sh 11-web-browser.sh 12-uncategorized.sh 13-unrepository-apps.sh 14-flatpak.sh 15-appearance.sh 16-config.sh
