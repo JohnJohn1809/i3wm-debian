6a0fc2a63b18c33b8cc6c4106fe5827eb7491773
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2021-12-30 16:06:07 -0300

    Update 01-sources-list.sh

--- scripts/01-sources-list.sh
+++ scripts/01-sources-list.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-sudo cp -R $HOME/i3wm-dotfiles/etc/apt/* "/etc/apt/"
+cd $HOME
+
+git clone https://github.com/JohnJohn1809/i3wm-dotfiles.git
+
+sudo cp -R $HOME/i3wm-dotfiles/debian/etc/apt/sources.list "/etc/apt/"
 
 sudo apt update
 
+cd $HOME/i3wm-debian/scripts/
