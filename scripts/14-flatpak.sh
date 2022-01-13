3ddea4ab1c71a2c4296374b6edafcc5f55e1d41e
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2022-01-13 08:42:20 -0300

    Update 14-flatpak.sh

--- scripts/14-flatpak.sh
+++ scripts/14-flatpak.sh
@@ -49,3 +49,13 @@ flatpak install flathub io.github.lainsce.Khronos
 flatpak install flathub com.rafaelmardojai.Blanket
 
 flatpak install flathub org.x.Warpinator
+
+flatpak install flathub com.github.gi_lom.dialect
+
+flatpak install flathub org.gnome.gitlab.YaLTeR.VideoTrimmer
+
+flatpak install flathub com.github.unrud.VideoDownloader
+
+flatpak install flathub com.github.tchx84.Flatseal
+
+#flatpak install flathub org.freefilesync.FreeFileSync
