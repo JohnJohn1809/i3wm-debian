13bac85e2632f257551aae05c836afce17019e52
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2022-09-07 02:55:25 -0300

    Update 14-flatpak.sh

--- scripts/14-flatpak.sh
+++ scripts/14-flatpak.sh
@@ -26,7 +26,7 @@ flatpak install flathub com.simplenote.Simplenote
 
 flatpak install flathub org.gnome.gitlab.somas.Apostrophe
 
-flatpak install org.gnome.gitlab.somas.Apostrophe.Plugin.TexLive
+#flatpak install org.gnome.gitlab.somas.Apostrophe.Plugin.TexLive
 
 # Image Editor
 
@@ -38,24 +38,34 @@ flatpak install org.gnome.gitlab.somas.Apostrophe.Plugin.TexLive
 
 flatpak install flathub io.github.Cockatrice.cockatrice
 
+# Multmidia
+
+flatpak install flathub io.bassi.Amberol
+
 # Other
 
 flatpak install flathub net.ankiweb.Anki
 
 flatpak install flathub com.github.gi_lom.dialect
 
-flatpak install flathub io.github.lainsce.Khronos
-
 flatpak install flathub com.github.unrud.VideoDownloader
 
 flatpak install flathub org.gnome.gitlab.YaLTeR.VideoTrimmer
 
-flatpak install flathub com.github.tchx84.Flatseal
+#flatpak install flathub com.github.tchx84.Flatseal
 
 flatpak install flathub com.rafaelmardojai.Blanket
 
-flatpak install flathub com.github.robertsanseries.ciano
+flatpak install flathub org.gabmus.whatip
 
-flatpak install flathub io.bassi.Amberol
+flatpak install flathub io.github.arunsivaramanneo.GPUViewer
+
+flatpak install flathub io.github.lainsce.Emulsion
+
+flatpak install flathub io.github.mmstick.FontFinder
 
 flatpak install flathub org.x.Warpinator
+
+#flatpak install flathub io.github.lainsce.Khronos
+
+#flatpak install flathub com.github.robertsanseries.ciano
