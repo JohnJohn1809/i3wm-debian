a02ef30171f89e81e5cc3b2fae79ed9ff93e586c
JohnJohn1809 <68306286+JohnJohn1809@users.noreply.github.com>
2023-03-20 04:46:56 -0300

    Update 14-flatpak.sh

--- scripts/14-flatpak.sh
+++ scripts/14-flatpak.sh
@@ -22,7 +22,7 @@ flatpak install flathub com.discordapp.Discord
 
 #flatpak install flathub io.typora.Typora
 
-flatpak install flathub com.simplenote.Simplenote
+#flatpak install flathub com.simplenote.Simplenote
 
 flatpak install flathub org.gnome.gitlab.somas.Apostrophe
 
@@ -40,33 +40,33 @@ flatpak install flathub org.gnome.gitlab.somas.Apostrophe
 
 # Multmidia
 
-flatpak install flathub io.bassi.Amberol
+#flatpak install flathub io.bassi.Amberol
 
 # Other
 
 flatpak install flathub net.ankiweb.Anki
 
-flatpak install flathub com.github.gi_lom.dialect
+#flatpak install flathub com.github.gi_lom.dialect
 
 flatpak install flathub com.github.unrud.VideoDownloader
 
-flatpak install flathub org.gnome.gitlab.YaLTeR.VideoTrimmer
+#flatpak install flathub org.gnome.gitlab.YaLTeR.VideoTrimmer
 
 #flatpak install flathub com.github.tchx84.Flatseal
 
-flatpak install flathub com.rafaelmardojai.Blanket
+#flatpak install flathub com.rafaelmardojai.Blanket
 
 flatpak install flathub org.gabmus.whatip
 
-flatpak install flathub io.github.arunsivaramanneo.GPUViewer
+#flatpak install flathub io.github.arunsivaramanneo.GPUViewer
 
 flatpak install flathub com.gitlab.newsflash
 
-flatpak install flathub io.github.lainsce.Emulsion
+#flatpak install flathub io.github.lainsce.Emulsion
 
-flatpak install flathub io.github.mmstick.FontFinder
+#flatpak install flathub io.github.mmstick.FontFinder
 
-flatpak install flathub net.daase.journable
+#flatpak install flathub net.daase.journable
 
 flatpak install flathub org.x.Warpinator
 
